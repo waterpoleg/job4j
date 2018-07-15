45e29aab5732c8a021080d73dba2d64776f586db
Oleg Korneev <waterpoleg@gmail.com>
2018-07-16 00:10:01 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate: print string to console
+* @author Oleg Korneev
+* @version 0.1
+* @since 05.01.2018
+*/
+
 public class Calculate {
+
+    /**
+    * Main method to print string with new line option
+    * @param args
+    */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+* @author Oleg Korneev (waterpoleg@gmail.com)
+* @version 0.1
+* @since 7.2018
+*/
+
+package ru.job4j;
