2d523f5f9a4aa2abc6b92641e2125a94805e90b3
Oleg Korneev <waterpoleg@gmail.com>
2018-07-20 18:25:34 +0300

    6.0. Заполнить массив степенями чисел.

---
+++ chapter_001/src/main/java/ru/job4j/calculate/array/Square.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculate.array;
+
+public class Square {
+
+    /**
+     * Метод заполняет массив числами от 1 до bound (включительно)
+     *
+     * можно сделать через Math.pow((i+1), 2), и привести к int,
+     * тк Math.pow возвращает double
+     * (i+1 тк индекс массива начинается с 0, а считать нужно с 1)
+     */
+    public int[] calc(int bound) {
+        int[] result = new int[bound];
+        for (int i = 0; i < bound; i++) {
+            result[i] = (i + 1) * (i + 1);
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/array/SquareTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculate.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SquareTest {
+
+    @Test
+    public void when6Then1to36() {
+        Square square = new Square();
+        int[] result = square.calc(6);
+        int[] test = {1, 4, 9, 16, 25, 36};
+        assertThat(result, is(test));
+    }
+}
