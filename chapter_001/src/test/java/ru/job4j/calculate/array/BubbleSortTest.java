c35d3547d01b17e58e19c12de34f33ed586cd71c
Oleg Korneev <waterpoleg@gmail.com>
2018-07-23 22:56:10 +0300

    6.5. Создать программу для сортировки массива методом перестановки. (refactoring)

--- chapter_001/src/test/java/ru/job4j/calculate/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/array/BubbleSortTest.java
@@ -9,25 +9,10 @@ public class BubbleSortTest {
     @Test
     public void whenSortByBubbleThenSortedArray() {
         BubbleSort array = new BubbleSort();
-
-        /**
-         * массив для сортировки
-         */
         int[] data = {8, 3, 10, 7, 1, 5, 4, 9, 2, 6};
-
-        /**
-         * сортируем
-         */
         int[] resultArray = array.sort(data);
-        /**
-         * ожидаемый результат - отсортированный масссив
-         */
-        int[] expectArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        /**
-         * првоеряем
-         */
-        assertThat(resultArray, is(expectArray));
+        int[] expectedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertThat(resultArray, is(expectedArray));
     }
 }
 
