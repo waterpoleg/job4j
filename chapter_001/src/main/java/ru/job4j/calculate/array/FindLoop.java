e878fa4c80ade1c4a51a8a7841a9a6f373d31a89
Oleg Korneev <waterpoleg@gmail.com>
2018-07-23 21:23:40 +0300

    6.1. Классический поиск перебором.

---
+++ chapter_001/src/main/java/ru/job4j/calculate/array/FindLoop.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate.array;
+
+public class FindLoop {
+    public int indexOf(int[] data, int el) {
+        int result = -1;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/array/FindLoopTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculate.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    /**
+     * ищем 9, которая стоит во 2 ячейке массива
+     */
+    @Test
+    public void when9Then2() {
+        FindLoop f = new FindLoop();
+        int[] data = {1, 4, 9, 16, 25, 6};
+        int i = f.indexOf(data, 9);
+        assertThat(i, is(2));
+    }
+
+    /**
+     * Если эелемент не найден по условию задачи возвращается значение -1
+     */
+    @Test
+    public void when8ThenMinusOne() {
+        FindLoop f = new FindLoop();
+        int[] data = {1, 4, 9, 16, 25, 6};
+        int i = f.indexOf(data, 8);
+        assertThat(i, is(-1));
+    }
+}
